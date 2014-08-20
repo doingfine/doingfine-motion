092aedbc90e290329ac7d491fc7f9658a701394d
Nelson Riley <nelsonriley@gmail.com>
2014-08-19 22:05:08 -0700

    (feat) Bind scope data to D3 directive

--- www/components/status/status.js
+++ www/components/status/status.js
@@ -5,13 +5,21 @@ angular.module('doingfine.status', [
   'service.d3'
 	])
 
-.controller('StatusController', function($scope, $rootScope, Device, d3Service) {
+.controller('StatusController', function($scope, $rootScope, $interval, Device, d3Service) {
   $scope.selectedFriend = $rootScope.selectedFriend || Device.user();
 
-  $scope.demoData = [10,20,30,40,60, 80, 20, 50];
+  // flip flop demo data to test binding D3 to scope
+  var a = [10,20,30,40,60, 80, 20, 50];
+  var b = [40,30,30,90,60, 40, 20, 20];
+  $scope.demoData = a;
+  $interval(function() {
+    if ($scope.demoData[0] === 10) {
+      $scope.demoData = b;
+    } else {
+      $scope.demoData = a;
+    }
+  }, 1000);
 
-  $scope.d3Service = d3Service;
-  
   $scope.hasFriends = true;
   if (Device.user().friends.length === 0) {
    $scope.hasFriends = false;
@@ -27,7 +35,5 @@ angular.module('doingfine.status', [
       $scope.safeModeIcon = onIcon;
     }
   };
-
-
 
 });
