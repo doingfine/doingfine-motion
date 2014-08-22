603df55489cbb5041ad691815554cfe50c7803fc
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-08-22 11:08:13 -0700

    (feat) Display current user as friend

--- www/components/menu/menu.js
+++ www/components/menu/menu.js
@@ -5,7 +5,8 @@ angular.module('doingfine.menu', [
 
 .controller('MenuController', function ($scope, $state, FriendsService, $rootScope, Device, API) {
   var currentUserId = Device.user()._id;
-  API.getAllFriends(currentUserId)
+  $scope.currentUserFirst = Device.user().first;
+  $scope.currentUserLast = Device.user().last;API.getAllFriends(currentUserId)
     .success(function (data) {
       $scope.friends = data;
     })
